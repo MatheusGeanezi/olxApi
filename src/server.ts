260e71271bb49c92d2a4f801c424c3eedbdb1f06
MatheusGeanezi <matheus.geanezi@yahoo.com.br>
2023-03-30 19:49:30 -0300

    chore: finish server and packages

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express , {Request, Response} from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
-
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
+import { mongoConnect } from './database/mongo';
 
 dotenv.config();
 
+mongoConnect();
+
 const server = express();
 
+server.use(cors());
 server.use(express.static(path.join(__dirname,'../public')));
-server.use(express.urlencoded({extended:true}));
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
+server.use(fileUpload());
 
 server.use((req: Request, res: Response) =>{
     res.status(404);
